ce51c3847af762e3ed4f3ff557aefa420b5fa09c
Helloysa Bezerra <helloysabezerra58@gmail.com>
2023-07-12 06:54:39 -0300

    Criando os packages 'app', 'app.view' e 'app.model', o arquivo FXML ConversorDeMoedasOverview, a classe MainApp e inicializando o arquivo FXML

---
+++ src/app/MainApp.java
@@ -0,0 +1,56 @@
+package app;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+	
+	public MainApp() {
+		
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Conversor de Moedas");
+		
+		showConversorDeMoedasOverview();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void showConversorDeMoedasOverview() {
+		
+		try {
+			//Carrega o arquivo FXML
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/ConversorDeMoedasOverview.fxml"));
+			rootLayout = (AnchorPane)loader.load();
+			
+			//Mostra a scene
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+}
